962e39dc5a3c5918e4752004155a2c24fe4f6773
BriceVaillant <naw.brice@gmail.com>
2025-06-06 11:04:56 +0200

    added className to prevent interference with other files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ export default function Navbar() {
     <nav>
       <div className="name">BV.</div>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/" className="navbarlink">Home</Link></li>
+        <li><Link to="/about" className="navbarlink">About</Link></li>
+        <li><Link to="/projects" className="navbarlink">Projects</Link></li>
+        <li><Link to="/contact" className="navbarlink">Contact</Link></li>
       </ul>
     </nav>
   );
