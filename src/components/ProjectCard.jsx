4d014467928438f580a476b96637f1885f6e9dde
BriceVaillant <naw.brice@gmail.com>
2025-06-13 11:06:32 +0200

    modified path

--- src/components/ProjectCard.jsx
+++
@@ -1,6 +0,0 @@
-// src/components/ProjectCard.js
-import './ProjectCard.css';
-
-export default function ProjectCard() {
-  return <h1>need to put card here</h1>;
-}
--- src/pages/RecipeApp.jsx
+++
@@ -1,6 +0,0 @@
-// src/pages/RecipeApp.js
-import './RecipeApp.css';
-
-export default function RecipeApp() {
-  return <h1>work in progress...</h1>;
-}
