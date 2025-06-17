f0c2aef82ddc7c333db989c206266a6d0a36c587
BriceVaillant <naw.brice@gmail.com>
2025-06-17 12:14:13 +0200

    fixed to prevent conflict between ESM syntax and commonJS

--- netlify/functions/getRecipes.js
+++ netlify/functions/getRecipes.js
@@ -1,26 +1,24 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI);
 
 const RecipeSchema = new mongoose.Schema({}, { collection: 'recipelist' });
 const Recipe = mongoose.model('Recipe', RecipeSchema);
 
-exports.handler = async function(request, context) {
+export async function handler(request, context) {
   try {
     const recipes = await Recipe.find({});
     return {
       statusCode: 200,
-      body: JSON.stringify(recipes)
+      body: JSON.stringify(recipes),
     };
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to fetch recipes' })
+      body: JSON.stringify({ error: 'Failed to fetch recipes' }),
     };
   }
-};
+}
