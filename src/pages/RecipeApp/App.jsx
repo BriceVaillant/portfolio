bcc1acbb714681c260c3443d08ef12d677ea0f14
BriceVaillant <naw.brice@gmail.com>
2025-06-25 12:11:28 +0200

    force login to use the app

--- src/pages/RecipeApp/App.jsx
+++ src/pages/RecipeApp/App.jsx
@@ -3,19 +3,28 @@ import Navbar from './RecipeAppNavbar';
 import RecipeHome from './RecipeHome';
 import RecipeList from './RecipeList';
 import './recipeapp.css';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function RecipeApp() {
   return (
-    <div id="RecipeApp">
-    <>
-      <Navbar />
-      <Routes>
-        <Route index element={<RecipeHome />} />
-        <Route path="home" element={<RecipeHome />} />
-        <Route path="recipelist" element={<RecipeList />} />
-      </Routes>
-    </>
-    </div>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: `${window.location.origin}/projects/recipe/recipelist`
+      }}
+    >
+      <div id="RecipeApp">
+        <>
+          <Navbar />
+          <Routes>
+            <Route index element={<RecipeHome />} />
+            <Route path="home" element={<RecipeHome />} />
+            <Route path="recipelist" element={<RecipeList />} />
+          </Routes>
+        </>
+      </div>
+    </Auth0Provider>
   );
 }
 
