943348e3468a24cb2974f4a5572dc5cb110d97a8
BriceVaillant <naw.brice@gmail.com>
2025-12-30 15:22:40 +0100

    fix import img

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,11 @@
 // src/pages/Projects.jsx
 import './Projects.css';
-import recipewebbapp from '/src/assets/RecipeWebApp.PNG';
+import recipewebbapp from '/src/assets/RecipeWebApp.png';
 import calculator from '/src/assets/calculator.PNG';
 import drummachine from '/src/assets/DrumMachine.PNG';
 import clock from '/src/assets/PomodoroClock.PNG';
 import quote from '/src/assets/randomquotegenerator.jpeg';
-import palindrome from '/src/assets/palindrome.PNG';
+import palindrome from '/src/assets/palindrome.png';
 import roman from '/src/assets/romannumberconverter.PNG';
 import treemap from '/src/assets/d3treemap.PNG';
 
@@ -13,35 +13,23 @@ import { Link } from 'react-router-dom';
 
 export default function Projects() {
   const handleMouseMove = (e) => {
-    //select card
     const card = e.currentTarget;
     const content = card.querySelector('.cardcontent');
-    //getBoundingClientRect get the size and position of the cards
-    //ex 100px from the top 50 from the left etc
+
     const rect = card.getBoundingClientRect();
 
-    //e.clientX give the position relative to the viewport
-    //-rect.left the substraction give you the position within the rectangle wich make
-    //x and y the pointer's position within the card
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    //this find the center of the card
     const centerX = rect.width / 2;
     const centerY = rect.height / 2;
 
-    //(y - centerY) / centerY) give a value between -1 and 1 depending on pointer position from the center
-    //the 10 is how much it will tilt 
     const rotateX = ((y - centerY) / centerY) * 5;
     const rotateY = ((x - centerX) / centerX) * 5;
 
-    //this is the calcul for the inner content
     const moveX = ((x - centerX) / centerX) * -5;
     const moveY = ((y - centerY) / centerY) * -5;
 
-    //card.style.transform influence the css of the card
-    //perspective(1000px) give the 3D effect
-    //rotate tilt the card
     card.style.transform = `perspective(1000px) rotateX(${-rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
     content.style.transform = `translateX(${moveX}px) translateY(${moveY}px)`;
   };
