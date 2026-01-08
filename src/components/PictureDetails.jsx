e7c9589fc2cce3ef8998da797539ff7063bf8466
BriceVaillant <naw.brice@gmail.com>
2026-01-08 18:50:35 +0100

    separated main image from sub-ones to get a better display of them in modal

--- src/components/PictureDetails.jsx
+++ src/components/PictureDetails.jsx
@@ -6,6 +6,11 @@ export default function PictureDetails({ image, onClose }) {
   const cloudinary_name = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
   const [images, setImages] = useState([]);
   const tag = `${image}`;
+  const mainImageUrl = `https://res.cloudinary.com/${cloudinary_name}/image/upload/v1767710557/IMG_${tag}_00.jpg`;
+  const mainImageKey = `IMG_${tag}_00`;
+
+  // need to fetch images with the given tag
+  //removed main image from the list to avoid duplication
 
   useEffect(() => {
     fetch(
@@ -22,28 +27,39 @@ export default function PictureDetails({ image, onClose }) {
       });
   }, [cloudinary_name, tag]);
 
+  const isSingle = images.length === 1;
+  //display main image on top and sub images below
+  // have sub images clickable to show in main view
   return (
-      <div className="pictures-modal" onClick={() => onClose()}>
-      {images.map((img) => {
-        const isSingle = images.length === 1;
-        const isMain = img.public_id.endsWith("_00");
-        return (
-          <div
-            className={`${
-              isSingle ? "single-view" : "multi-view"
-            }`}
-            key={img.public_id}
-          >
-            <img
-              className={isMain ? "main-picture" : "sub-picture"}
-              src={`https://res.cloudinary.com/${cloudinary_name}/image/upload/v${img.version}/${img.public_id}.${img.format}`}
-              alt={img.public_id}
-              width={img.width}
-              height={img.height}
-            />
-          </div>
-        );
-      })}
+    <div className="pictures-modal" onClick={() => onClose()}>
+      <div className={`${isSingle ? "single-view" : "multi-view"}`} key={tag}>
+        <img src={mainImageUrl} alt={mainImageKey} />
+        {images.map((img) => {
+          const isMain = img.public_id.endsWith("_00");
+          return (
+            <div>
+              <div className={`${isMain ? "single-view" : "d-none"}`}>
+                <img
+                  className={isMain ? "main-picture" : "d-none"}
+                  src={`https://res.cloudinary.com/${cloudinary_name}/image/upload/v${img.version}/${img.public_id}.${img.format}`}
+                  alt={img.public_id}
+                  width={img.width}
+                  height={img.height}
+                />
+              </div>
+              <div>
+                <img
+                  className={isMain ? "d-none" : "sub-picture"}
+                  src={`https://res.cloudinary.com/${cloudinary_name}/image/upload/v${img.version}/${img.public_id}.${img.format}`}
+                  alt={img.public_id}
+                  width={img.width}
+                  height={img.height}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
