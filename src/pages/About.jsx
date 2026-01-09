b43d08fc843071c8831ca62c50e9a895ec8aedd0
BriceVaillant <naw.brice@gmail.com>
2026-01-09 14:38:33 +0100

    fix about page phrasing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 // src/pages/About.jsx
-import './About.css';
-import profilpic from '../assets/profilepicture.jpg';
-import { Link } from 'react-router-dom';
+import "./About.css";
+import profilpic from "../assets/profilepicture.jpg";
+import { Link } from "react-router-dom";
 
 export default function About() {
   return (
@@ -11,11 +11,29 @@ export default function About() {
       </div>
       <div className="description">
         <h1>Développeur Junior</h1>
-        <p>Pour une personne en constante quête de connaissances, le développement constitue un domaine infini et véritablement passionnant.</p>
-        <p className="insta"> En dehors du code, je me consacre à la peinture. Retrouvez mes plus belles miniatures sur mon <a href="https://www.instagram.com/briceminis/" className="social-link" target="_blank" rel="noopener noreferrer">Instagram</a> où sur ce <Link className="social-link" to="/briceminis">site.</Link></p>
+        <p>
+          Pour une personne en constante quête de connaissances, le
+          développement constitue un domaine infini et véritablement
+          passionnant.
+        </p>
+        <p className="insta">
+          {" "}
+          En dehors du code, je me consacre à la peinture. Retrouvez mes plus
+          belles miniatures{" "}
+          <Link className="social-link" to="/briceminis">
+            ici
+          </Link>{" "}
+          ou sur mon{" "}
+          <a
+            href="https://www.instagram.com/briceminis/"
+            className="social-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram.
+          </a>
+        </p>
       </div>
     </div>
   );
 }
-
-
